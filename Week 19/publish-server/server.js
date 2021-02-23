3bc0a34ace28681f18dbcc628b57f0ecc46715d2
hu.kunyu@shipout.cn <hu.kunyu@shipout.cn>
2021-02-23 11:12:32 +0800

    feat: Week 19 登录实例代码完善

--- Week 19/publish-server/server.js
+++ Week 19/publish-server/server.js
@@ -1,12 +1,83 @@
 let http = require('http')
+let https = require('https')
 let unzipper = require('unzipper')
+let querystring = require('querystring')
 
+// 2. 在publish-server中的auth路由下：接收code，使用code+client_id+client_secret换token
+function auth (request, response) {
+    let query = querystring.parse(request.url.match(/^\/auth\?([\s\S]+)$/)[1])
+    getToken(query.code, info => {
+        console.info(info)
+        // response.write(JSON.stringify(info))
+        response.write(`<a href='http://localhost:8083/?token=${info.access_token}'>publish</a>`)
+        response.end()
+    })
+    // console.info(query)
+}
+
+function getToken (code, cb) {
+    let request = https.request({
+        hostname: 'github.com',
+        path: `/login/oauth/access_token?code=${code}&client_id=Iv1.cc3912ed18bb926d&client_secret=787a69db33c7ea0a5749c5a9aa4926c3f1266f6b`,
+        port: 443,
+        method: 'POST'
+    }, response => {
+        body = ''
+        response.on('data', chunk => {
+            body += (chunk.toString())
+        })
+        response.on('end', chunk => {
+            cb(querystring.parse(body))
+        })
+    })
+
+    request.end()
+}
+// 4. 在publish-server中的publish路由： 用token获取用户信息，检查权限，接受发布
+function publish (request, response) {
+    let query = querystring.parse(request.url.match(/^\/publish\?([\s\S]+)$/)[1])
+    getUser(query.token, userInfo => {
+        console.info('userInfo', userInfo)
+        if (userInfo.login === 'bug-hky') {
+            response.write(JSON.stringify(userInfo))
+            // let outFile = fs.createWriteStream('../server/public/tmp.zip')
+            // request.pipe(outFile)
+            request.pipe(unzipper.Extract({ path: '../server/public/' }))
+            request.on('end', () => {
+                response.end('success!!!')
+            })
+        }
+    })
+}
+
+function getUser (token, cb) {
+    let request = https.request({
+        hostname: 'api.github.com',
+        path: `/user`,
+        port: 443,
+        method: 'GET',
+        headers: {
+            Authorization: `token ${token}`,
+            "User-Agent": 'example-publish'
+        }
+    }, response => {
+        let body = ''
+        response.on('data', chunk => {
+            body += (chunk.toString())
+        })
+        response.on('end', chunk => {
+            cb(JSON.parse(body))
+        })
+    })
+
+    request.end()
+}
 
 http.createServer(function(request, response) {
     console.info('request', request.headers)
-    
-    // let outFile = fs.createWriteStream('../server/public/tmp.zip')
-    // request.pipe(outFile)
-
-    request.pipe(unzipper.Extract({ path: '../server/public/' }))
+    if (request.url.match(/^\/auth\?/)) {
+        auth(request, response)
+    } else if (request.url.match(/^\/publish\?/)) {
+        publish(request, response)
+    }
 }).listen(8082)
--- Week 19/publish-tool/publish.js
+++ Week 19/publish-tool/publish.js
@@ -1,7 +1,45 @@
 let http = require('http')
 let fs = require('fs')
 let archiver = require('archiver')
+let child_process = require('child_process')
+let querystring = require('querystring')
 
+// 1. 打开https://github.com/login/oauth/authorize
+child_process.exec(`start https://github.com/login/oauth/authorize?client_id=Iv1.cc3912ed18bb926d`)
+
+
+// 3. 在publish-tool中创建server，接收token，点击发布
+http.createServer(function(request, response) {
+    let query = querystring.parse(request.url.match(/^\/\?([\s\S]+)$/)[1])
+    publish(query.token)
+}).listen(8083)
+
+
+function publish (token) {
+    let request = http.request({
+        postname: '127.0.0.1',
+        port: 8082, // 8882
+        method: 'POST',
+        path: `/publish?token=${token}`,
+        headers: {
+            'Content-Type': 'application/octet-stream'
+        }
+    }, response => {
+        console.info('>>>????', response)
+    })
+
+    const archive = archiver('zip', {
+        zlib: { level: 9 } //Sets the compression level
+    })
+    
+    archive.directory('./sample/', false)
+    
+    archive.finalize()
+    
+    archive.pipe(request)
+}
+
+/*
 let request = http.request({
     postname: '127.0.0.1',
     port: 8082, // 8882
@@ -25,3 +63,5 @@ archive.finalize()
 
 // archive.pipe(fs.createWriteStream("tmp.zip"))
 archive.pipe(request)
+
+*/
